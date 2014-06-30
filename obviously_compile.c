bd319a89875ac5967552061772dbdc99f517c492
colin_g <colin_g@epitech.eu>
2014-06-30 13:05:11 +0200

    Just strange first time i saw it ! :)

---
+++ obviously_compile.c
@@ -0,0 +1,18 @@
+/*
+** test.c for colin_g in /home/colin_g
+**
+** Made by julien colin
+** Login  <colin_g@epitech.eu>
+**
+** Started on  lun. juin 30 13:00:36 2014 julien colin
+** Last update lun. juin 30 13:04:21 2014 julien colin
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    http://www.google.com
+    printf("Hello, World!\n");
+    return (0);
+}
